2641b056c8341fb9d97aa87e72694a05d9cd1429
jrh-w <jakub.r.wlodarz@gmail.com>
2023-06-02 23:48:58 +0200

    feat: Working model v1

---
+++ skrypt.py
@@ -0,0 +1,127 @@
+# Importy do sprawdzania modelu na podstawie zdjęcia
+import os
+import tensorflow as tf
+import object_detection
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+#os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] ='false'
+#os.environ['XLA_PYTHON_CLIENT_ALLOCATOR']='platform'
+#os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+tf.config.experimental.set_visible_devices([], 'GPU')
+
+CUSTOM_MODEL_NAME = 'resnet'
+LABEL_MAP_FILE = 'label_map.pbtxt'
+
+paths = {
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+ }
+
+files = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_FILE)
+}
+
+# Plik konfiguracyjny do ponownego załadowania
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Odtwórz najbardziej zaawansowany checkpoint modelu
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-5')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+# Odtworzenie etykiet
+category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+
+# Wykrywanie pojedynczego zdjęcia - linijki 51 - 85
+"""
+# Ścieżka do wybranego zdjęcia
+IMAGE_PATH = os.path.join(paths['IMAGE_PATH'], 'y.jpg')
+
+# Skrypt do odczytu zdjęcia
+img = cv2.imread(IMAGE_PATH)
+image_np = np.array(img)
+
+input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+detections = detect_fn(input_tensor)
+
+num_detections = int(detections.pop('num_detections'))
+detections = {key: value[0, :num_detections].numpy()
+              for key, value in detections.items()}
+detections['num_detections'] = num_detections
+
+# detection_classes should be ints.
+detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+label_id_offset = 1
+image_np_with_detections = image_np.copy()
+
+viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes']+label_id_offset,
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=5,
+            min_score_thresh=.8,
+            agnostic_mode=False)
+
+plt.imshow(cv2.cvtColor(image_np_with_detections, cv2.COLOR_BGR2RGB))
+plt.savefig("mygraph.png")
+"""
+
+# Skrypt do odczytu wideo z ciągłą detekcją
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    image_np = np.array(frame)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=5,
+                min_score_thresh=.8,
+                agnostic_mode=False)
+
+    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
